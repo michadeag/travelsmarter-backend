5632fa1da153f7709c69762b4969a17c5b98fcd9
michadeag <michadeag@gmail.com>
2026-06-06 23:46:22 +0200

    Add test endpoint to contact routes for debugging

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -6,6 +6,14 @@ const { sendContactEmail } = require('../controllers/contactController');
  * Contact form routes
  */
 
+// Test endpoint - verify routes are loading
+router.get('/test', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Contact routes are loading!'
+  });
+});
+
 // POST /api/contact/send - Send contact form email
 router.post('/send', sendContactEmail);
 
